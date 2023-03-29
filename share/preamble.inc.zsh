3ceabb0ef1c0a4fe916857638cf5b8203cc5d8d3
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2023-03-29 14:48:52 +0059

    fix

--- share/preamble.inc.zsh
+++ share/preamble.inc.zsh
@@ -81,12 +81,11 @@ fi
 typeset -gU fpath FPATH path PATH
 
 # Autoload via fpath, not direct paths
-autoload -z $ZIQDIR/functions/*~*~(.N:t) \
+autoload -z $ZIQDIR/functions/*~(*~|*/THROW|*/CATCH)(.N:t) \
             $ZIQDIR/functions/*/*~(*~|*/ok/qlocal)(.N:t2) \
-            $ZIQDIR/bin/*~*~(.N:t) \
-            THROW CATCH #zsweep:pass
+            $ZIQDIR/bin/*~*~(.N:t)
 
-builtin autoload +zX ok/qlocal
+builtin autoload +zX ok/qlocal THROW CATCH
 
 #
 # Simple, small support messaging system
